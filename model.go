b578b8c920902e149ba6472761180caf2bd04ddb
unknown <huynhtuananh2005@gmail.com>
2026-04-21 11:14:55 +0700

    fix created at in model

--- model.go
+++ model.go
@@ -1,14 +1,12 @@
 package main
 
-import "time"
-
 type Product struct {
-	ID          int64     `json:"id"`
-	ProductCode string    `json:"product_code" binding:"required,min=3,max=20"`
-	Name        string    `json:"name" binding:"required,min=2,max=100"`
-	Price       float64   `json:"price" binding:"required,gt=0"`
-	Quantity    int       `json:"quantity" binding:"gte=0"`
-	Category    string    `json:"category" binding:"required"`
-	Description string    `json:"description"`
-	CreatedAt   time.Time `json:"created_at"`
+	ID          int64   `json:"id"`
+	ProductCode string  `json:"product_code" binding:"required,min=3,max=20"`
+	Name        string  `json:"name" binding:"required,min=2,max=100"`
+	Price       float64 `json:"price" binding:"required,gt=0"`
+	Quantity    int     `json:"quantity" binding:"gte=0"`
+	Category    string  `json:"category" binding:"required"`
+	Description string  `json:"description"`
+	CreatedAt   string  `json:"created_at"`
 }
